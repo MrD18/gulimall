4bc62f1646326a9f5e0d8de4740e2c1725ba8e3b
duhao <duhao@ysstech.com>
2020-05-13 15:09:48 +0800

    项目初始化创建

---
+++ gulimall-order/src/test/java/com/yss/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yss.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
